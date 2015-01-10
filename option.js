06a3a94c7623b75b6a6a49cfc7eb3ca72dd89ea3
dylandy.chang <steven80919@gmail.com>
2015-01-10 22:30:49 +0800

    added jquery and remove to use chrome storage using localstorage to store signature

--- option.js
+++ option.js
@@ -1,26 +1,11 @@
-var save_local = function(key , val){
-  chrome.storage.local.set({
-    key : val
-  },
-    function(){
-      console.log("saved"+key+"as"+val);
-    }
-  );
-}
-
-var get_local = function(target){
-  var output;
-  chrome.storage.local.get( target , function(result){
-    output = result.target;
-  });
-  return output;
-}
-
 var set_signature = function(){
-
+  $('button').click(function(){
+    localStorage.setItem("signature" , $('textarea').val());
+  });
 }
+
 
 jQuery(function($){
+  $('textarea').val( localStorage.signature );
   set_signature();
-
 });
