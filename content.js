8caa915457b8e1b977398a71903426b0119491fb
dylandy.chang <steven80919@gmail.com>
2015-01-12 18:10:27 +0800

    added jquery address lib and new icons

--- content.js
+++ content.js
@@ -1,7 +1,15 @@
+var check_if_url_change = function(){
+  var url_path = window.location.pathname;
+  localStorage.setItem("");
+}
+
+
 jQuery(function($){
-  chrome.storage.sync.get( "signature" , function(result){
-    $('textarea').val(result.signature);
-    }
-  );
+  $.address.change(function(e){
+    chrome.storage.sync.get( "signature" , function(result){
+      $('textarea').val(result.signature);
+      }
+    );
+  });
 });
 
