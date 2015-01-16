9438b95e436d7ee2a18fb8bd88fbcb968491bd95
dylandy.chang <steven80919@gmail.com>
2015-01-16 18:27:29 +0800

    tried to fixed content script with click event, it worked sometimes....hmmm

--- content.js
+++ content.js
@@ -1,15 +1,27 @@
-var check_if_url_change = function(){
-  var url_path = window.location.pathname;
-  localStorage.setItem("");
+var sleep = function(n){
+  var   start = new Date().getTime();
+  while(true) if(new Date().getTime()-start> n)   break;
 }
 
-
 jQuery(function($){
-  $.address.change(function(e){
+  $("#side .item").click(function(){
+    console.log("in 1");
     chrome.storage.sync.get( "signature" , function(result){
       $('textarea').val(result.signature);
-      }
-    );
+    });
+  });
+  $(".list > div > a").click(function(){
+    sleep(2000);
+    console.log("in 2");
+    chrome.storage.sync.get( "signature" , function(result){
+      $('.float-comment textarea').val(result.signature);
+    });
+  });
+  $(".list > div:eq(2)").children('button').click(function(){
+    console.log("in 3");
+    chrome.storage.sync.get( "signature" , function(result){
+      $('textarea').val(result.signature);
+    });
   });
 });
 
