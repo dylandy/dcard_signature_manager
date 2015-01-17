3dbe6715f973d62d88d69b8d2ad08558739cc961
dylandy.chang <steven80919@gmail.com>
2015-01-17 20:26:31 +0800

    renewed icons from origin dcard's to self manufactured

--- content.js
+++ content.js
@@ -3,25 +3,19 @@ var sleep = function(n){
   while(true) if(new Date().getTime()-start> n)   break;
 }
 
+var get_signature = function(){
+  chrome.storage.sync.get( "signature" , function(result){
+    $('textarea').val(result.signature);
+  });
+}
+
 jQuery(function($){
-  $("#side .item").click(function(){
-    console.log("in 1");
-    chrome.storage.sync.get( "signature" , function(result){
-      $('textarea').val(result.signature);
-    });
-  });
-  $(".list > div > a").click(function(){
+  get_signature();
+  $("body").on("click" , "#side item" , get_signature);
+  $("body").on("click" , ".list > div > a" , function(){
     sleep(2000);
-    console.log("in 2");
-    chrome.storage.sync.get( "signature" , function(result){
-      $('.float-comment textarea').val(result.signature);
-    });
+    get_signature();
   });
-  $(".list > div:eq(2)").children('button').click(function(){
-    console.log("in 3");
-    chrome.storage.sync.get( "signature" , function(result){
-      $('textarea').val(result.signature);
-    });
-  });
+  $("body").on("click" , ".list > div:eq(2) > button" , get_signature);
 });
 
