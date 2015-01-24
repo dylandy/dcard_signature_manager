13c5ee53f9937bd80c8f0b85ad3db186563866b1
dylandy.chang <steven80919@gmail.com>
2015-01-25 00:20:23 +0800

    reorganised the code to much more agile

--- content.js
+++ content.js
@@ -4,31 +4,21 @@ var get_signature = function(){
   });
 }
 
+var get_signature_to_comment = function(){
+  chrome.storage.sync.get( "signature" , function(result){
+    $('.float-comment textarea').val(result.signature);
+  });
+}
+
 jQuery(function($){
   $("body").on("click" , "#side item" , get_signature);
   $("body").on("click" , ".list > div > a" , function(){
-    setTimeout(function() {
-      chrome.storage.sync.get( "signature" , function(result){
-        $('.float-comment textarea').val(result.signature);
-      });
-    }, 2000);
+    setTimeout(function() {get_signature_to_comment()}, 2000);
   });
-  $("body").on("click" , ".list > div:eq(2) > button" , function(){
-    chrome.storage.sync.get( "signature" , function(result){
-      $('.float-comment textarea').val(result.signature);
-    });
-  });
-  $("body").on("click" , ".float-ops button" , function(){
-    chrome.storage.sync.get( "signature" , function(result){
-      $('.float-comment textarea').val(result.signature);
-    });
-  });
+  $("body").on("click" , ".list > div:eq(2) > button" , get_signature_to_comment);
+  $("body").on("click" , ".float-ops button" , get_signature_to_comment);
   $("body").on("click" , ".navbar-right:eq(2)" , function(){
-    setTimeout(function(){
-      chrome.storage.sync.get( "signature" , function(result){
-        $('.float-comment textarea').val(result.signature);
-      });
-    } , 2000);
+    setTimeout(function(){get_signature_to_comment()} , 2000);
   });
 });
 
