b14d584592f19effda0b85fc7b526ae17bc12382
dylandy.chang <steven80919@gmail.com>
2015-01-24 17:23:35 +0800

    removed sleep function and kindly solved re-edit error

--- content.js
+++ content.js
@@ -1,8 +1,3 @@
-var sleep = function(n){
-  var   start = new Date().getTime();
-  while(true) if(new Date().getTime()-start> n)   break;
-}
-
 var get_signature = function(){
   chrome.storage.sync.get( "signature" , function(result){
     $('textarea').val(result.signature);
@@ -10,13 +5,17 @@ var get_signature = function(){
 }
 
 jQuery(function($){
-  get_signature();
   $("body").on("click" , "#side item" , get_signature);
   $("body").on("click" , ".list > div > a" , function(){
     setTimeout(function() {
       get_signature();
     }, 2000);
   });
-  $("body").on("click" , ".list > div:eq(2) > button" , get_signature);
+  $("body").on("click" , ".list > div:eq(2) > button" , function(){
+    chrome.storage.sync.get( "signature" , function(result){
+      $('.float-comment textarea').val(result.signature);
+    });
+  });
+  $("body").on("click" , ".float-ops button" , get_signature);
 });
 
